32f3f3553509636d077b3614044d94fb2b7d4527
dhanashri2004 <bhandaredhanashri638@gmail.com>
2025-12-25 16:15:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll (HR-safe, no animation overload)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
